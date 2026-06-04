873af48b388dedacd9b4aa7e436ba33ae41b34f0
jovanni0 <jovanni0@proton.me>
2026-06-04 14:44:35 +0300

    disabled CORS

--- COD/WEBSERVRS/backend/src/main.ts
+++ COD/WEBSERVRS/backend/src/main.ts
@@ -8,19 +8,17 @@ async function bootstrap()
 {
     const app = await NestFactory.create(AppModule);
     
+    // For usage with an external app, CORS needs to be disabled. 
     app.enableCors({
-        origin: [
-            'http://localhost:5173',      // dev browser
-            'http://localhost',           // Capacitor Android
-            'capacitor://localhost',      // Capacitor iOS
-        ],
-        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
+        origin: true,
+        methods: '*',
         credentials: true,
     });
-    // Activează validarea automată pentru toate endpoint-urile
+
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     
     await app.listen(3000);
 }
+
+
 bootstrap();
